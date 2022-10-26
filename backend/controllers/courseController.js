71894a4bb513fa73fde0552b17c0b0556fa89a27
Adam Nizar <adoom.nizar2905@gmail.com>
2022-10-26 19:04:32 +0200

    Adam Adds Course Details option

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -31,9 +31,33 @@ const updateCourse = (req,res)=>{
 
 //create new course
 const createCourse = async (req,res)=>{
-    const{title,hours} = req.body
+    const{
+        title,
+        hours,
+        subject,
+        price,
+        discount,
+        discountValidUntil,
+        instructor,
+        summary,
+        previewURL,
+        outline,
+        subtitles} = req.body
     try{
-        const course = await Course.create({title,hours,subject,price,discount,discountValidUntil,instructor,summary,previewURL,reveiws,overallRating,ratings,outline,subtitles})
+        const course = await Course.create({
+            title,
+            hours,
+            subject,
+            price,
+            discount,
+            discountValidUntil,
+            instructor,
+            summary,
+            previewURL,
+            reveiws,
+            overallRating,
+            ratings,outline,
+            subtitles})
         //Course.create() is async that's why we put async around the handler fn, so u can use await right here
         //now we're storing the response of Course.create() (which is the doc created along with its is) in course
         //inside create, u pass thru an object representing the doc u wanna create
