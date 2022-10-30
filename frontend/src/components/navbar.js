fb369f21a53409bdc286c5674f96e9a7938bcf0c
Adam Nizar <adoom.nizar2905@gmail.com>
2022-10-30 14:30:48 +0200

    Country select

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -4,6 +4,16 @@ const Navbar = () => {
     <header>
       <div className="container">
         <h1>Online Learning System</h1>
+
+        <div>
+          <h4>Select Country</h4>
+           <select>
+  <option value="egypt" selected="selected">Egypt</option>
+  <option value="USA" selected="selected">United States of America </option>
+  <option value="Germany" selected="selected">Germany</option>
+  <option value="UAE" selected="selected">United Arab Emiratetes</option>
+          </select>
+      </div>
       </div>
     </header>
   );
